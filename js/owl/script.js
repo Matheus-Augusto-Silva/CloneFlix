d18284453a405eb8f523e5cfaa7a22210616b178
Matheus <matheusaugusto-18@hotmail.com>
2021-03-20 21:03:42 -0300

    Update script.js

--- js/owl/script.js
+++ js/owl/script.js
@@ -2,3 +2,15 @@ let maisInformacoes = document.getElementById("informacoes");
 function clicarInformacoes() {
   maisInformacoes.style.display = "block";
 }
+
+//pegando todas as imagens dentro da div com classe .slider
+var slider = document.querySelectorAll(".slider img");
+
+var imagens_slider = [];
+
+//varrendo array e pegando imagens do src para um novo array
+for (cont = 0; cont < slider.length; cont++) {
+  imagens_slider.push(slider[cont].src);
+}
+
+console.log(imagens_slider);
